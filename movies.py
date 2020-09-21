6607be3ccb3c84c3ed47311643aa941be861b04f
cyberjive <jputte49@hotail.com>
2020-09-20 20:26:12 -0500

    updates

--- movies.py
+++ movies.py
@@ -1,29 +1,31 @@
 from datetime import datetime
 
+from flask import make_response, abort
+
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
 # Temp Data to serve
 MOVIES = {
-    "1": {
+    1: {
         "mname": "Looper",
         "mgenre": "Scifi",
         "mdatereleased": "09-28-2012",
         "mruntime": 118,
     },
-    "2": {
+    2: {
         "mname": "Children of Men",
         "mgenre": "Scifi",
         "mdatereleased": "09-03-2006",
         "mruntime": 109,
     },
-    "3": {
+    3: {
         "mname": "Pulp Fiction",
         "mgenre": "Pulp?",
         "mdatereleased": "05-21-1994",
         "mruntime": 154,
     },
-    "4": {
+    4: {
         "mname": "Reservoir Dogs",
         "mgenre": "Crime",
         "mdatereleased": "10-09-1992",
@@ -32,7 +34,7 @@ MOVIES = {
 }
 
 # Create a handler for our read (GET) movies
-def read():
+def read_all():
     """
     This function responds to a request for /api/movies
     with the complete lists of movies
@@ -41,3 +43,41 @@ def read():
     """
     # Create the list of movies from our data
     return [MOVIES[key] for key in sorted(MOVIES.keys())]
+
+def read_one(id):
+    """
+    This function responds to a request for api/peole/{id}
+    with one matching movie from movies
+
+    :param id: ID of movie in database
+    :type: int
+    :return: movie matching ID
+    :rtype: movie
+    """
+    # existence check
+    if id in MOVIES.keys():
+        movie = MOVIES.get(ID)
+    else:
+        abort(
+            404, f'Movie with ID of { id } not found.'
+        )
+    return movie
+
+def create(movie):
+    """
+    This function creates a new movie in the movie DB
+    based on the passed in movie data
+
+    :param movie:  movie to create in movie structure
+    :return:        201 on success, 406 on movie exists
+    """ 
+    mname = movie.get("mname", None)
+
+    if movie not in MOVIES and mname is not None:
+        MOVIES.update(movie)
+        return MOVIES[movie.key], 201
+    else:
+        abort(
+            406,
+            f'{ movie } already exists.'
+        )
